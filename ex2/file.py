8c453069215ef665482ba8330ea9f4786f486dff
ibrohim021 <dim_aviasky@mail.ru>
2022-09-29 20:26:32 +0300

    ex2

---
+++ ex2/file.py
@@ -0,0 +1,14 @@
+# Напишите программу, которая принимает на вход число N
+# и выдает набор произведений чисел от 1 до N.
+# пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+from math import factorial
+
+
+n = int(input('Введите число: '))
+
+factorial = 1
+for i in range(1,n+1):
+    factorial *= i
+    print(factorial, end=' ')
+
